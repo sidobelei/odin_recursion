097f3e65f1f6556bbd66952fb6ed848e26d672d0
Brandy Tang <126715380+branmuffin2020@users.noreply.github.com>
2024-06-27 12:50:24 -0700

    Add fibs_rec method
    This function provides the fibonacci sequence
    recursively.

--- fibonacci.rb
+++ fibonacci.rb
@@ -16,3 +16,24 @@ def fibs(length)
     end
     return array
 end
+
+def fibs_rec(length, array = []) 
+    if length == 0
+        return array
+    end
+    if length == 1
+        array.push(0)
+        return [0]
+    elsif length == 2
+        array.push(0)
+        array.push(1)
+        return [0, 1]
+    else
+        length -= 1
+        array = fibs_rec(length, array)
+        f0 = array[length - 2]
+        f1 = array[length - 1]
+        array.push(f0 + f1)
+        return array   
+    end
+end
